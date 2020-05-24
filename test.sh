7ff6064561721b5fc2e3e39f3f0169243b6e1cf0
Simon Evans <si@si.org>
2020-05-24 18:25:18 +0100

    Add GitHub actions for testing
    - Add an action for testing on macOS which uses the default image with
      xcode and swift already installed.
    
    - For Linux, add a Dockerfile that pulls down a container image with
      swift installed.

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+MACOS_TESTS="Passed"
+LINUX_TESTS="Passed"
+
+swift test -c release || MACOS_TESTS="Failed"
+docker build --tag=foundation-benckmark-tests:$(date +%s) . || LINUX_TESTS="Failed"
+
+echo
+echo macOS tests ${MACOS_TESTS}
+echo Linux tests ${LINUX_TESTS}
